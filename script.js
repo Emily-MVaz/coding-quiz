afeab104240b3b1aa50d9fc12df644dd6dd12130
Emily Vazquez <emily.m.vazquez99@gmail.com>
2022-03-08 17:22:28 -0600

    initial commit

---
+++ script.js
@@ -0,0 +1,44 @@
+// const
+const startButton = document.getElementById("start");
+const submitButton = document.getElementById("submit");
+const quizContainer = document.getElementById("quiz");
+const showResults = document.getElementById("results");
+
+//var
+
+
+
+//Questions
+const myQuestions = [
+    {
+       question: "" ,
+       answers: {
+           a: "",
+           b: "",
+           c: ""
+       }
+       correctAnswer: ""
+    }
+]
+
+}
+
+
+// Functions
+
+//making the quiz
+function quizStart(){
+
+}
+
+
+//showing the result
+function quizResults(){
+
+}
+
+
+// show results when submit is clicked
+submitButton.addEventListener("click", showResults)
+
+//
