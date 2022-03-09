fe1b0b89cef95df5f46479fe7346201bd68aeb7e
Emily Vazquez <emily.m.vazquez99@gmail.com>
2022-03-09 17:29:52 -0600

    Fixed radio button answers now turn green when correct  or red when incorrect  also fixed some typos

--- assets/js/script-1.js
+++ assets/js/script-1.js
@@ -13,12 +13,13 @@ var myQuestions = [
            b: "var a = 'abcde'",
            c: "var a = true "
        },
+       
        correctAnswer: "b"
     },
     {
         question: "Which of the following is a single line comment in Javascript? ",
         answers: {
-            a: "<!-- This is a comment -->",
+            a: "This is a comment",
             b: "/* This is a comment */",
             c: "//This is a comment"
         },
@@ -51,11 +52,19 @@ function startQuiz(questions, quizContainer, showResults, submitButton){
 
                 //add a radio button
                 answers.push(
-                    `<label>
-                    <input type="radio" name="question ${i}" value="${letter}">
-                    ${letter} :
-                    ${ questions[i].answers[letter]}
-                    </label>`
+                    
+                    '<label>'
+                        + '<input type="radio" name="question'+i+'" value="'+letter+'">'
+                        + letter + ': '
+                        + questions[i].answers[letter]
+				    + '</label>'
+                
+                    
+                    // `<label>
+                    // <input type="radio" name="question ${i}" value="${letter}">
+                    // ${letter} :
+                    // ${ questions[i].answers[letter]}
+                    // </label>`
                 );
             }
 
@@ -64,11 +73,12 @@ function startQuiz(questions, quizContainer, showResults, submitButton){
 
                 //add questions and add answers
                 //template literal
-                `<div class="question"> ${questions[i].question} </div>
-                <div class="answers"> ${answers.join('')} </div>` 
+
+                // `<div class="question"> ${questions[i].question} </div>
+                // <div class="answers"> ${answers.join('')} </div>` 
                 
-                // '<div class="question">' + questions[i].question + '</div>'
-                // + '<div class="answers">' + answers.join('') + '</div>'
+                '<div class="question">' + questions[i].question + '</div>'
+                + '<div class="answers">' + answers.join('') + '</div>'
 
             );
         }
@@ -83,13 +93,13 @@ function startQuiz(questions, quizContainer, showResults, submitButton){
         // put together all answers
         var answerContainers = quizContainer.querySelectorAll(".answers")
 
-        var userAnswer = " ";
+        var userAnswer = "";
         var numCorrect = 0;
 
         for(var i=0; i<questions.length; i++){
 
             //find the chosen answer
-            userAnswer = (answerContainers[i].querySelectorAll("input[name=question" + i + "]:checked") || {})
+            userAnswer = (answerContainers[i].querySelector("input[name=question" + i + "]:checked") || {}).value;
 
             // var selector = `input[name=question${i}]:checked`;
             // userAnswer = (answerContainer[i].querySelector(selector) || {}).value;
@@ -100,11 +110,11 @@ function startQuiz(questions, quizContainer, showResults, submitButton){
                 //number of correct answers goes up
                 numCorrect++;
 
-                //asnwers turn green
+                //answers turn green
                 answerContainers[i].style.color = "green";
             }
 
-            //if the answer is wrong
+            //if the answer is wrong or left blank
             else{
                 
                 //answers turn red
